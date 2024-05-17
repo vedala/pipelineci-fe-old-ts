53cd2d07248463ca4760c54ee5b8ab3a56377e21
Kalyan Vedala <svedala23@gmail.com>
2024-05-16 21:50:37 -0700

    Use router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import PageNotFound from './components/PageNotFound';
+import LoginButton from './components/login';
+import LogoutButton from './components/logout';
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        { !isAuthenticated
+          ? <LoginButton />
+          : <LogoutButton />
+        }
+      </div>
+      <Routes>
+        <Route path="/" element={<Dashboard />}/>
+        <Route path="*" element={<PageNotFound />}/>
+      </Routes>
+    </Router>
   );
 }
 
